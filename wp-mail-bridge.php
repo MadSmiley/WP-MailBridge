5feb21f1477362251727d6a68f2aca612337390e
MadSmiley <germain.belacel@gmail.com>
2025-10-18 13:47:36 +0200

    Init

---
+++ wp-mail-bridge.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: WP MailBridge
+ * Plugin URI: https://github.com/MadSmiley/WP-MailBridge
+ * Description: MailBridge is a WordPress plugin that centralizes the management of personalized transactional emails.
+ * Version: 1.0.0
+ * Author: MadSmiley
+ * Author URI: https://www.linkedin.com/in/germain-belacel/
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wp-mail-bridge
+ * Domain Path: /languages
+ *
+ * @package WP_MailBridge
+ */
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
